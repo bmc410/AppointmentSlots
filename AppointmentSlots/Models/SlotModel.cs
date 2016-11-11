17062eb613324bef3039823380e689fd2a9accfe
bmc410 <bmc410@comcast.net>
2016-11-11 15:06:12 -0500

    Initial Checkin

---
+++ AppointmentSlots/Models/SlotModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSlots.Models
+{
+    public class SlotModel
+    {
+        public DateTime StartSlot { get; set; }
+        public DateTime EndSlot { get; set; }
+    }
+}
