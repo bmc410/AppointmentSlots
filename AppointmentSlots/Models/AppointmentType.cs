26a6f9bf6e479e8cb6eb3b6c655289d8ef38d42b
bmc410 <bmc410@WILLIAMMCCOB41C>
2016-11-11 19:41:35 -0500

    Add project files.

---
+++ AppointmentSlots/Models/AppointmentType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentSlots.Models
+{
+    public class AppointmentType
+    {
+        public int ApptTypeId { get; set; }
+        public String ApptType { get; set; }
+        public String ApptDesc { get; set; }
+        public int ApptLength { get; set; }
+    }
+}
