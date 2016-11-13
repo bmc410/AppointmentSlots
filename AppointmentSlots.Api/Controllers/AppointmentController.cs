6c0e1bd10ffdd2742af8a8e3723e67ffedd18031
bmc410 <bmc410@WILLIAMMCCOB41C>
2016-11-12 22:26:08 -0500

    fixed API

--- AppointmentSlots.Api/Controllers/AppointmentController.cs
+++ AppointmentSlots.Api/Controllers/AppointmentController.cs
@@ -18,6 +18,98 @@ namespace AppointmentSlots.Api.Controllers
         List<ApptAvailability> apptAvailability;
         List<AppointmentType> appointmentTypes;
 
+
+        static List<ApptModel> GetAppointments(DateTime apptDate)
+        {
+            using (ApptEntities db = new ApptEntities())
+            {
+                List<ApptModel> appts = new List<ApptModel>();
+                appts = (from a in db.usp_GetAppointmentsByDay(apptDate)
+                         select new ApptModel
+                         {
+                             StartTime = (DateTime)a.StartTime,
+                             EndTime = (DateTime)a.EndTime
+                         }).ToList();
+
+
+                return appts;
+            }
+        }
+
+        private void Setup()
+        {
+            apptAvailability = GetApptAvailability();
+            apptExceptions = GetApptExceptions();
+            appointmentTypes = GetApptTypes();
+        }
+
+        // GET api/Appointment/GetSlots/30
+        public List<SlotModel> GetSlots(int duration)
+        {
+            using (ApptEntities db = new ApptEntities())
+            {
+                List<SlotModel> slots = new List<SlotModel>();
+                slots = (from a in db.usp_GetAppointmentSlots(new TimeSpan(6, 0, 0),
+                             new TimeSpan(20, 0, 0), duration, DateTime.Now)
+                         select new SlotModel
+                         {
+                             StartSlot = (DateTime)a.start,
+                             EndSlot = (DateTime)a.end,
+                         }).ToList();
+
+                slots = slots.Where(x => x.StartSlot.TimeOfDay >= DateTime.Now.TimeOfDay).ToList();
+                return slots;
+            }
+
+        }
+
+        // GET api/Appointment/GetApptTypes
+        public List<AppointmentType> GetApptTypes()
+        {
+            using (ApptEntities db = new ApptEntities())
+            {
+                return (from a in db.usp_GetApptTypes()
+                        select new AppointmentType
+                        {
+                            ApptDescription = a.ApptDescription,
+                            ApptLength = (int)a.ApptLength,
+                            ApptType = a.ApptType,
+                            ApptTypeId = a.ApptTypeId
+                        }).ToList();
+            }
+        }
+
+        static List<ApptAvailability> GetApptAvailability()
+        {
+            using (ApptEntities db = new ApptEntities())
+            {
+                return (from a in db.usp_GetAvailability()
+                        select new ApptAvailability
+                        {
+                            AvailabilityStartDateTime = (DateTime)a.StartDateTime,
+                            AvailabilityEndDateTime = (DateTime)a.EndDateTime,
+                            AvailabilityId = a.AvailabilityId,
+                            DayOfWeek = (int)a.DayOfWeek
+                        }).ToList();
+            }
+        }
+
+        static List<ApptException> GetApptExceptions()
+        {
+            using (ApptEntities db = new ApptEntities())
+            {
+                return (from a in db.usp_GetExceptionTimes()
+                        select new ApptException
+                        {
+                            ExceptionEndDateTime = (DateTime)a.ExceptionEndDate,
+                            ExceptionStartDateTime = (DateTime)a.ExceptionStartDate,
+                            ExceptionId = a.ExceptionId,
+                            IsRecurring = (bool)a.IsRecurring
+                        }).ToList();
+            }
+        }
+
+        // GET api/Appointment/GetAppointments
         public List<ApptModel> GetAppointments()
         {
             using (ApptEntities db = new ApptEntities())
